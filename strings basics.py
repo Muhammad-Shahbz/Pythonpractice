bdde7b04be2639bf2d1ce86b2b03ef5870b3ebc3
Muhammad-Shahbz <optm.muhammad@gmail.com>
2025-12-17 18:26:28 +0100

    Pythonpractice

---
+++ strings basics.py
@@ -0,0 +1,21 @@
+print("hello world") # this is a basic print statment showing how to print in python
+# A string is a squence of characters enclosed within single quotes
+
+a= "hello" #this is a string variable
+
+b = """im learning python from basic to advance level 
+as a key to become artificial intelligence engineer""" #this is multiline strings
+print(a)
+print(b)
+
+#String indexing
+
+s = "Optometrist"
+
+print(s[0])
+print(s[-1])
+print(s[:-1]) #print all except last character
+print(s[2:]) #print from index 2 to end
+print(s[::3]) #print every third character
+
+
