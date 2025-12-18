7c79129e389fe2f8718f95fd237f9baa26301617
Muhammad-Shahbz <optm.muhammad@gmail.com>
2025-12-18 15:54:44 +0100

    Pythonpractice

--- strings basics.py
+++ strings basics.py
@@ -90,4 +90,65 @@ ap = 'python'
 print(ap.find('t')) # it will return index of character 't' which is 2
 
 
+#index() is similar to find() but it raises an error  if the substring is not found
+str5 = "artificial intelligence"
+print(str5.index('i')) # it will return index of first 'i' which is 3
 
+# rindex() function gives the index of last occurence of substring like this
+str6 = 'programming in python'
+print(str6.rindex('r'))
+
+# count() function counts the number of occurrence of substring in string
+str7 = "data science is getting disappear and take over by artifiical intelligence"
+print(str7.count('is')) # it will return 2 becausse 'is' occurs 2 times in string
+
+# checking boolean methods
+str8 = "HELLO"
+print(str8.isupper()) # it will return true because all characters are in uppercase
+
+'hello'.islower() # it will return true because all characters are in lowercase
+
+# isalnum() function checks if all character in string are alphanumeric
+
+str9 = "hello124"
+print(str9.isalnum()) # it will return true because all characters are alphanumeric
+str10 = "hello@12"
+print(str10.isalnum()) # it will return false because '@' is not alphanumeric
+print("hello")
+# isspace() function checks if all characters in string are with space or not 
+print(" ".isspace()) # it will retrun true
+print("hellowoeld".isspace())
+
+# istitle() functions checks if string is in title case or not
+str11 = "Hello World"
+print(str11.istitle()) # it will return true because both words start with uppercasse
+
+# white space removal functions
+str12 = " hello world"
+print(str12.strip())# it will remove leading and tailing white spaces
+
+
+# Isstrip() funnction removes leading white spaces
+str13 = "   hello world"
+print(str13.lstrip()) 
+
+#rstrip() function removes tailing white spaces
+str14 = "hello world "
+print(str14.rstrip())
+
+# replace() function replace substring with another substring
+
+"I like java".replace("java", "python")
+# Split() function splits string into list of substrings based on values provided
+str15 = "python is easy to learn"
+print(str15.split()) # it will split string at space and return list of words
+
+# rsplit() function splilts string from right side based on value provided 
+str16 = "apple, bannana, orange"
+print(str16.rsplit(", ")) # it will split string at ',' and return list of fruits
+
+#join() function joins elements of iterable to end of string
+str17 = ("-".join(["2025", "12", "18"]))
+print(str17)
+
+print(str18
