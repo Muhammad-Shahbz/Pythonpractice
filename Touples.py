43aa7cf1eab55bb5a6b775aed099bdf2908d865a
Muhammad-Shahbz <optm.muhammad@gmail.com>
2025-12-18 22:08:26 +0100

    Pythonpractice

---
+++ Touples.py
@@ -0,0 +1,14 @@
+### Touples in Python
+#Touples are similar to list but they are immutable means we cannot change the values once touple is created .Touples are created using parenthesis (). 
+# Touples are faster than list because of immutability.
+# touples can store all types of data like list
+#in Touples we can use duplicate values
+#Touples support indexing and slicing like list
+# Touples can be nested like list
+# In touples we can do concatenation and repitation like list
+# in touples we can use membership operator like list
+#in touples we can use built in functions like list
+# in touples we can convert list to touple and touple to list
+# in touples we can unpack values like list
+# in touples we can iterate using loops like list
+
