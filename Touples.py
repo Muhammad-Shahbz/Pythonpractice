8023c62381ab7009a958906eb03ff673314a7514
Muhammad-Shahbz <optm.muhammad@gmail.com>
2025-12-18 22:18:29 +0100

    Pythonpractice

--- Touples.py
+++ Touples.py
@@ -11,4 +11,24 @@
 # in touples we can convert list to touple and touple to list
 # in touples we can unpack values like list
 # in touples we can iterate using loops like list
+#touples are used when we want to store data that should not be change throughout the programe
+# in touples commas are used to separate vlaues
+# the differece between touple and sting is that string is used to store squence of characters while touple is used to store sequence of any type of data.
+
+
+# Norma Touple
+t = (10,20,30,40,50)
+print(t)
+
+t = 10, 20, 30
+print(t)  # without parenthesis also touple can be created
+ #single element touple
+t1 = (20,) #comma is necesary to create single element touple
+print(t1)
+
+# Touples with different data types
+t2 = (10, "hello", 20.5, True)
+print(t2)
+
+
 
