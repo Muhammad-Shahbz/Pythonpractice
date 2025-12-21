0d65d50882e1cfccc219c749f99de05b27d7b6ef
Muhammad-Shahbz <optm.muhammad@gmail.com>
2025-12-21 17:16:38 +0100

    new

---
+++ Loops.py
@@ -0,0 +1,43 @@
+#Loops are used to repeate task
+# two types
+# for and while loop
+#while loop
+#while condition:
+    #some work
+count = 1 
+while count <= 5 :
+    print("hello", count)
+    count +=1
+    
+# print numbers 1 to 5
+i = 1
+while i <=5 :
+    print(i)
+    i +=1
+print("loop ended")
+
+#Print numbers from 1 to 10
+i = 1
+while i <= 10:
+    print(i)
+    i +=1
+    
+#print numbers from 1 to 100
+x =1
+while x <=100:
+    print(x)
+    x +=1
+print("loop ended")
+
+#print numbers from 100 to 1
+v = 100
+while v >=1:
+    print(v)
+    v -=1
+    
+# print the multiplication of a number n
+b = 5
+while b <=50:
+    print(b)
+    b *=5
+    
--- P.dictionaries.py
+++ P.dictionaries.py
@@ -6,3 +6,28 @@
 Person = {"name": "Sara", "age": 25}
 print(Person)
 print(Person["name"])
+print(Person["age"])
+print(Person.get("age"))
+print(Person.get("name"))
+
+#adding new key
+d1 = {"city": "islamabad", "population": 250000}
+print(d1["city"])
+d1["country"]="Pakistan"
+print(d1)
+
+#update existing key
+d1.update({"city": "lahore"})
+print(d1)
+
+#Removing items
+d1.pop("population")
+print(d1)
+del d1["city"]
+print(d1)
+
+d1.update({"cityy": "Multan", "population": 30000000})
+print(d1.keys()) # return all keys
+print(d1.values()) #return all values
+print(d1.items()) # returns key value pairs
+print(d1.copy())
